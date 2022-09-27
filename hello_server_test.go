846fdac1c22fd4815ad6d29380e9f896dd8d4333
Shaurya Kalia <113332561+shaurya-harness@users.noreply.github.com>
2022-09-27 14:01:57 +0530

    Update hello_server_test.go

--- hello_server_test.go
+++ hello_server_test.go
@@ -3,21 +3,21 @@ package main
 import "testing"
 
 func TestGreetingSpecificJohn(t *testing.T) {
-	t.Errorf("Greeting was incorrect, got: %s, want: %s.", greeting, "Hello, John\n")
+	t.Errorf("Greeting was incorrect, got: %s, want: %s.", "Hello, John\n")
 }
 
 func TestGreetingSpecificDemo(t *testing.T) {
-	t.Errorf("Greeting was incorrect, got: %s, want: %s.", greeting, "Hello, Demo\n")
+	t.Errorf("Greeting was incorrect, got: %s, want: %s.", "Hello, Demo\n")
 }
 
 func TestShowFailure(t *testing.T) {
-	t.Errorf("Intentional failure. got: %s, want: %s.", greeting, "Hello, Demo\n")
+	t.Errorf("Intentional failure. got: %s, want: %s.", "Hello, Demo\n")
 }
 
 
 
 func TestGreetingDefault(t *testing.T) {
-	t.Errorf("Greeting was incorrect, got: %s, want: %s.", greeting, "Hello, Guest\n")
+	t.Errorf("Greeting was incorrect, got: %s, want: %s.", "Hello, Guest\n")
 }
  
 
