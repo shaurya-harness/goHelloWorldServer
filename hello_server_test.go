13b20cc0b7728a22b713b13b03a90dc912b03560
Shaurya Kalia <113332561+shaurya-harness@users.noreply.github.com>
2022-09-27 13:07:46 +0530

    Update hello_server_test.go

--- hello_server_test.go
+++ hello_server_test.go
@@ -3,10 +3,7 @@ package main
 import "testing"
 
 func TestGreetingSpecificJohn(t *testing.T) {
-	greeting := CreateGreeting("John")
-	if greeting != "Hello, John\n" {
-		t.Errorf("Greeting was incorrect, got: %s, want: %s.", greeting, "Hello, John\n")
-	}
+	t.Errorf("Greeting was incorrect, got: %s, want: %s.", greeting, "Hello, John\n")
 }
 
 func TestGreetingSpecificDemo(t *testing.T) {
